2a2db48d19a1018249271ac53b8b430c4e549799
Jem <jemstobart@hotmail.com>
2025-02-19 15:03:38 +0000

    Update github actions build file and fix threading

--- main.py
+++ main.py
@@ -21,75 +21,83 @@ JIRA_EMAIL = os.getenv('JIRA_EMAIL')
 JIRA_TOKEN = os.getenv('JIRA_TOKEN')
 JIRA_PROJECT_KEY = os.getenv('JIRA_PROJECT_KEY')
 
+stop_flag = False
+
 def process_message():
-    try:
-        #Set SQS client
-        sqs = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY,
-                           aws_secret_access_key=SECRET_ACCESS_KEY)
+    # Set SQS client
+    sqs = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY,
+                       aws_secret_access_key=SECRET_ACCESS_KEY)
 
-        #Receive message
-        response = sqs.receive_message(QueueUrl=P2_QUEUE, MessageAttributeNames=['All'],
-                                       MaxNumberOfMessages=1, WaitTimeSeconds=20)
+    # Receive message
+    response = sqs.receive_message(QueueUrl=P2_QUEUE, MessageAttributeNames=['All'],
+                                   MaxNumberOfMessages=1, WaitTimeSeconds=20)
 
-        messages = response.get('Messages')
+    messages = response.get('Messages')
 
-        #If there are messages in the queue
-        if messages is not None:
-            #Get first message
-            message = messages[0]
-            logger.info(f"Message received from queue with ID: {message["MessageId"]}")
+    # If there are messages in the queue
+    if messages is not None:
+        # Get first message
+        message = messages[0]
+        logger.info(f"Message received from queue with ID: {message["MessageId"]}")
 
-            #Get message body
-            body = json.loads(message['Body'])
+        # Get message body
+        body = json.loads(message['Body'])
 
-            #Validate body
-            if "title" in body and "description" in body:
-                if body["title"] and body["description"]:
+        # Validate body
+        if "title" in body and "description" in body:
+            if body["title"] and body["description"]:
 
-                    #Set JIRA client
-                    jira = Jira(
-                        url=JIRA_URL,
-                        username=JIRA_EMAIL,
-                        password=JIRA_TOKEN,
-                        cloud=True
-                    )
+                # Set JIRA client
+                jira = Jira(
+                    url=JIRA_URL,
+                    username=JIRA_EMAIL,
+                    password=JIRA_TOKEN,
+                    cloud=True
+                )
 
-                    #Create issue
-                    logger.info("Attempting to create issue")
+                # Create issue
+                logger.info("Attempting to create issue")
 
-                    jira.create_issue(fields={
-                        'project': {'key': JIRA_PROJECT_KEY},
-                        'issuetype': {
-                            "name": "Task"
-                        },
-                         'summary': body["title"],
-                         'description': body["description"],
-                    })
-                    logger.info(f"Jira task created, payload: {json.dumps({
-                        'project': {'key': JIRA_PROJECT_KEY},
-                        'issuetype': {
-                            "name": "Task"
-                        },
-                         'summary': body["title"],
-                         'description': body["description"],
-                    })}")
-                #If body is incorrect display errors
-                else:
-                    logger.error("Either the title or description or both are empty")
+                jira.create_issue(fields={
+                    'project': {'key': JIRA_PROJECT_KEY},
+                    'issuetype': {
+                        "name": "Task"
+                    },
+                    'summary': body["title"],
+                    'description': body["description"],
+                })
+                logger.info(f"Jira task created, payload: {json.dumps({
+                    'project': {'key': JIRA_PROJECT_KEY},
+                    'issuetype': {
+                        "name": "Task"
+                    },
+                    'summary': body["title"],
+                    'description': body["description"],
+                })}")
+            # If body is incorrect display errors
             else:
-                logger.error("Either the title or description or both are missing from the SQS message")
-
-            #Delete message from queue
-            sqs.delete_message(
-                QueueUrl=P2_QUEUE,
-                ReceiptHandle=message['ReceiptHandle']
-            )
-            logger.info(f"Message deleted from queue with ID: {message["MessageId"]}")
-        #If no messages then display that
+                logger.error("Either the title or description or both are empty")
         else:
-            logger.info("No messages in queue")
-    except Exception as e:
-        logger.error(f"An error occurred {e}")
+            logger.error("Either the title or description or both are missing from the SQS message")
+
+        # Delete message from queue
+        sqs.delete_message(
+            QueueUrl=P2_QUEUE,
+            ReceiptHandle=message['ReceiptHandle']
+        )
+        logger.info(f"Message deleted from queue with ID: {message["MessageId"]}")
+    # If no messages then display that
+    else:
+        logger.info("No messages in queue")
+
+
+def process_message_outer():
+    global stop_flag
+    while not stop_flag:
+        try:
+            process_message()
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
 
 def background_thread():
     thread = threading.Thread(target=process_message, daemon=True)
